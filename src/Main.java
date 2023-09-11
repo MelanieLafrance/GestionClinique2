90b87f55a51c77222062fbed1f884114dc71e44c
Hugo Pélissier-Jolin <hugo.pelissier-jolin.1@ulaval.ca>
2023-09-11 19:21:26 -0400

    creation of template

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import enums.TriageType;
+
+public class Main {
+    public static void main(String[] args) {
+        // Ceci n'est pas un test!! C'est un exemple d'utilisation.
+        TriageType doctorTriageType = TriageType.FIFO;
+        TriageType radiologyTriageType = TriageType.FIFO;
+
+        // Clinic clinic = new Clinic(doctorTriageType, radiologyTriageType /* , ... */)
+        // clinic.triagePatient("John", 4, enums.VisibleSymptom.MIGRAINE);
+    }
+}
